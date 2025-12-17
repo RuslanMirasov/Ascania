155bcf0518e29443b1302155a954e1128982777e
Ruslan <info@mirasov.dev>
2025-12-17 16:05:57 +0100

    change map to image

--- assets/js/geography.js
+++ assets/js/geography.js
@@ -1,48 +1,5 @@
-// export const initGeography = () => {
-//   if (!window.ymaps3) {
-//     console.warn('Yandex Maps API not loaded');
-//     return;
-//   }
-
-//   ymaps3.ready.then(async () => {
-//     const { YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer, YMapMarker } = ymaps3;
-
-//     const map = new YMap(document.getElementById('geography'), {
-//       location: {
-//         center: [79.442692, 57.570922],
-//         zoom: 4,
-//       },
-//       zoomRange: { min: 3, max: 19 },
-//     });
-
-//     // Базовые слои карты
-//     map.addChild(new YMapDefaultSchemeLayer());
-//     map.addChild(new YMapDefaultFeaturesLayer());
-
-//     // Загружаем маркеры
-//     const markersData = await fetch('./../assets/json/markers.json').then(r => r.json());
-
-//     markersData.points.forEach(point => {
-//       const container = document.createElement('div');
-
-//       const img = document.createElement('img');
-//       img.src = './../assets/img/pin.svg';
-//       img.style.width = '90px';
-//       img.style.height = '77px';
-//       img.style.transform = 'translate(-50%, -100%)';
-
-//       container.appendChild(img);
-
-//       const marker = new YMapMarker({ coordinates: point.coordinates }, container);
-
-//       map.addChild(marker);
-//     });
-//   });
-// };
-
 export const initGeography = () => {
   if (!window.ymaps3) {
-    console.warn('Yandex Maps API not loaded');
     return;
   }
 
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,7 +3,7 @@ import { hidePreloader, initNavigationMenu } from './helpers.js';
 import { initSliders, onHeroSlideChange } from './sliders.js';
 import { initScrollToBlock } from './scrollToBlock.js';
 import { initInputMasks } from './inputMasks.js';
-import { initYandexMap } from './yandex-map.js';
+//import { initYandexMap } from './yandex-map.js';
 import { initGeography } from './geography.js';
 import { initTabs } from './tabs.js';
 import { initParallax } from './parallax.js';
@@ -15,7 +15,7 @@ initNavigationMenu();
 initSliders();
 initScrollToBlock();
 initInputMasks();
-initYandexMap();
+//initYandexMap();
 initGeography();
 initTabs();
 
